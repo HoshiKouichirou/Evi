1b183ca102fefda69c3d554036b5058092b991d6
poyo_gamer <32663150+poyo555@users.noreply.github.com>
2018-09-06 13:27:30 +0900

    バグありバージョン

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 $(function() {
-  var point = 1;
   var score = 0;
+  var point = 1;
+
   function scoreup() {
     $(".score").text(score += point)//スコアカウント
   }
   function gameover() {
     console.log("over")
-    $(".p-start").text("GAME OVER")
-    $(".start").toggleClass("remove");//ゲームオーバー処理
+    $(".p-start").text("GAME OVER");
+    $(".p-start").remove();
+    $(".start").toggleClass("remove");
+    $(".start").toggleClass("start");//ゲームオーバー処理
   }
   function gameclear() {
     console.log("clear")
-    $(".p-start").text("GAME CLEAR")
-    $(".start").toggleClass("remove");//ゲームクリア処理
+    $(".p-start").text("GAME CLEAR");
+    $(".p-start").toggleClass("p-start");
+    $(".start").toggleClass("remove");
+    $(".start").toggleClass("start");//ゲームクリア処理
   }
   function result() {//ゲームリザルト
     console.log("re")
     clearInterval(point);
-    if(point < 600000){
+    if(score > 600000){
       gameclear()
     }
     else {
       gameover()
     }
   }
-
   $(".p-start").click(function() {
     $(".start").toggleClass("remove");
     var point = setInterval(scoreup, 1);
   });
-
   $(".buy").click(function() {
     result()
     console.log("good")
