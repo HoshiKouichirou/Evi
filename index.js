3eeea6c723fa3696453304b6077a939deeba1ad1
poyo_gamer <32663150+poyo555@users.noreply.github.com>
2018-09-08 13:43:55 +0900

    good

--- index.js
+++ index.js
@@ -1,37 +1,76 @@
 $(function() {
-  var point = 1;
   var score = 0;
+  var point = 1;
+  var index = 0;
+  var enemy = 0;
   function scoreup() {
     $(".score").text(score += point)//スコアカウント
   }
+  function gamestart() {
+    for (var i = 0; i < 10; i++) {
+      if (i < 5) {
+        $(".enemies").append("<div class='enemy-left " + i + " '></div>")
+      }
+      else {
+        $(".enemies").append("<div class='enemy-right " + i + " '></div>")
+      }
+    }
+  }
+  function enemymotion() {
+    
+  }
   function gameover() {
     console.log("over")
-    $(".p-start").text("GAME OVER")
-    $(".start").toggleClass("remove");//ゲームオーバー処理
+    $(".p-start").text("GAME OVER");
+    $(".restart").toggleClass("remove");
+    $(".p-start").toggleClass("p-start");
+    $(".start").toggleClass("remove");
+    $(".start").toggleClass("start");//ゲームオーバー処理
   }
   function gameclear() {
     console.log("clear")
-    $(".p-start").text("GAME CLEAR")
-    $(".start").toggleClass("remove");//ゲームクリア処理
+    $(".p-start").text("GAME CLEAR");
+    $(".restart").toggleClass("remove");
+    $(".p-start").toggleClass("p-start");
+    $(".start").toggleClass("remove");
+    $(".start").toggleClass("start");//ゲームクリア処理
   }
   function result() {//ゲームリザルト
     console.log("re")
     clearInterval(point);
-    if(point < 600000){
+    if(score > 600000){
       gameclear()
     }
     else {
       gameover()
     }
   }
-
+  // function restart() {
+  //   score = 0
+  //   index = 0
+  //   $(".play").toggleClass("start");
+  //   $(".p-play").toggleClass("p-start");
+  //   $(".restart").toggleClass("remove");　後回し
+  //   $(".p-play").text("START");
+  //
+  // }
+  // $(".restart").click(function() {
+  //   restart()
+  //   console.log("hi")
+  // })
   $(".p-start").click(function() {
-    $(".start").toggleClass("remove");
-    var point = setInterval(scoreup, 1);
+    if (index === 0) {
+      index = 1;
+      $(".start").toggleClass("remove");
+      var point = setInterval(scoreup, 1);
+      gamestart();
+    }
   });
-
   $(".buy").click(function() {
     result()
     console.log("good")
+  })
+  $(".hide").click(function() {
+
   })
 }())
